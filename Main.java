293cd2bf40e2d09869efc4bb1a9d7589e479a0f9
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-26 17:52:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int diameter =  0;
+    int radius = 4;
+    int area = 0;
+    int c = 0;
+    double PI = Math.PI;
+    System.out.println("area = " + (PI * (radius * radius)));
+  }
+}
+/* Todo:
+-equation for circumfernce
+-save radius as d/2
+- add text box 
+-let user input diameter
+- make look nice*/
